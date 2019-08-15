de393e9cc471a0f046b1fdcb2d426a59c7e4869f
odoo <falconsoft.3d@gmail.com>
2019-08-14 20:18:08 -0400

    Iniciando con el Front

--- apps/erp/views.py
+++ apps/erp/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from __future__ import unicode_literals
 
-# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate
+from django.shortcuts import redirect
+from django.contrib.auth import logout, login
+
+# Public
+def login_user(request):
+    return render(request, "login.html")
