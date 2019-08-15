a512ea32d170cf04f47526cdc61edb028d96756f
odoo <falconsoft.3d@gmail.com>
2019-08-14 21:48:55 -0400

    Iniciando con el Front

--- pyerp/urls.py
+++ pyerp/urls.py
@@ -20,5 +20,5 @@ from django.conf.urls import url, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^', include ('apps.home.urls')),
-    url(r'^erp/', include ('apps.erp.urls')),
+    # url(r'^erp/', include ('apps.erp.urls')),
 ]
