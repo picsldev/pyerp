eefadb9989277f7cb0abff9b27637d2a0d343da5
carmelo_lopez <carmelolopez3@hotmail.com>
2019-08-16 16:42:36 -0400

    Agregadas vistas de usuarios.

--- apps/base/urls.py
+++ apps/base/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from ..base.views import PartnerListView, PartnerDetailView, PartnerCreateView, PartnerUpdateView, DeletePartner
 from ..base.views import CompanyListView
 from ..base.views import ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, DeleteProduct
+from ..base.views import UserListView, UserDetailView, UserCreateView, UserUpdateView, DeleteUser, ChangePasswordForm, DoChangePassword
 
 urlpatterns = [
+    path('users', UserListView.as_view(), name='users'),
+    path('user/add/', UserCreateView.as_view(), name='user-add'),
+    path('user/<int:pk>/', UserDetailView.as_view(), name='user-detail'),
+    path('user/<int:pk>/update', UserUpdateView.as_view(), name='user-update'),
+    path('user/<int:pk>/delete/', DeleteUser, name='user-delete'),
+    path('user/change-password/<int:pk>', ChangePasswordForm, name='password-change'),
+    path('user/change-password-confirm/<int:pk>', DoChangePassword, name='do-change-password'),
+
     path('products', ProductListView.as_view(), name='products'),
     path('product/add/', ProductCreateView.as_view(), name='product-add'),
     path('product/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
--- apps/base/views.py
+++ apps/base/views.py
@@ -3,7 +3,100 @@ from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate
 from ..base.models import PyPartner, PyCompany, PyProduct
+from django.contrib.auth.models import User
+
+
+class UserListView(ListView):
+    model = User
+    template_name = 'erp/list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(UserListView, self).get_context_data(**kwargs)
+        context['title'] = 'Usuarios'
+        context['detail_url'] = 'user-detail'
+        context['add_url'] = 'user-add'
+        context['fields'] = [
+            {'string': 'Login', 'field': 'username'},
+            {'string': 'Nombre', 'field': 'first_name'},
+            {'string': 'Apellido', 'field': 'last_name'},
+            {'string': 'Email', 'field': 'email'},
+        ]
+        return context
+
+
+class UserDetailView(DetailView):
+    model = User
+    template_name = 'erp/detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(UserDetailView, self).get_context_data(**kwargs)
+        context['title'] = context['object'].username
+        context['breadcrumbs'] = [{'url': 'users', 'name': 'Usuarios'}]
+        context['update_url'] = 'user-update'
+        context['delete_url'] = 'user-delete'
+        context['fields'] = [
+            {'string': 'Nombre', 'field': 'first_name'},
+            {'string': 'Apellido', 'field': 'last_name'},
+            {'string': 'Login', 'field': 'username'},
+            {'string': 'Email', 'field': 'email'},
+        ]
+        context['buttons'] = [
+            {
+                'act': reverse('password-change', kwargs={'pk': context['object'].pk}),
+                'name': 'Cambiar contraseña',
+                'class': 'success'
+            }
+        ]
+        return context
+
+
+class UserCreateView(CreateView):
+    model = User
+    fields = ['username', 'email', 'first_name', 'last_name', 'password']
+    template_name = 'erp/form.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(UserCreateView, self).get_context_data(**kwargs)
+        context['title'] = 'Crear Usuario'
+        context['breadcrumbs'] = [{'url': 'users', 'name': 'Usuarios'}]
+        context['back_url'] = reverse('users')
+        return context
+
+
+class UserUpdateView(UpdateView):
+    model = User
+    fields = ['username', 'email', 'first_name', 'last_name']
+    template_name = 'erp/form.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(UserUpdateView, self).get_context_data(**kwargs)
+        context['title'] = context['object'].username
+        context['breadcrumbs'] = [{'url': 'users', 'name': 'Usuarios'}]
+        context['back_url'] = reverse('user-detail', kwargs={'pk': context['object'].pk})
+        return context
+
+
+@login_required(login_url="/erp/login")
+def DeleteUser(self, pk):
+    user = User.objects.get(id=pk)
+    user.delete()
+    return redirect(reverse('users'))
+
+
+def ChangePasswordForm(self, pk):
+    return render(self, 'erp/change_password.html', {'pk': pk})
+
+
+def DoChangePassword(self, pk, **kwargs):
+    user = User.objects.get(id=pk)
+    if user and self.POST['new_password1'] == self.POST['new_password2']:
+        user.set_password(self.POST['new_password1'])
+    else:
+        return render(self, 'erp/change_password.html', {'pk': pk, 'error': 'Las contraseñas no coinciden.'})
+    return redirect(reverse('user-detail', kwargs={'pk': pk}))
+
 
 
 class PartnerListView(ListView):
@@ -52,6 +145,7 @@ class PartnerCreateView(CreateView):
         context = super(PartnerCreateView, self).get_context_data(**kwargs)
         context['title'] = 'Crear partner'
         context['breadcrumbs'] = [{'url': 'partners', 'name': 'Partners'}]
+        context['back_url'] = reverse('partners')
         return context
 
 
@@ -64,6 +158,7 @@ class PartnerUpdateView(UpdateView):
         context = super(PartnerUpdateView, self).get_context_data(**kwargs)
         context['title'] = context['object'].name
         context['breadcrumbs'] = [{'url': 'partners', 'name': 'Partners'}]
+        context['back_url'] = reverse('partner-detail', kwargs={'pk': context['object'].pk})
         return context
 
 
@@ -140,6 +235,7 @@ class ProductCreateView(CreateView):
         context = super(ProductCreateView, self).get_context_data(**kwargs)
         context['title'] = 'Crear producto'
         context['breadcrumbs'] = [{'url': 'products', 'name': 'Productos'}]
+        context['back_url'] = reverse('products')
         return context
 
 
@@ -152,6 +248,7 @@ class ProductUpdateView(UpdateView):
         context = super(ProductUpdateView, self).get_context_data(**kwargs)
         context['title'] = context['object'].name
         context['breadcrumbs'] = [{'url': 'products', 'name': 'Productos'}]
+        context['back_url'] = reverse('product-detail', kwargs={'pk': context['object'].pk})
         return context
 
 
