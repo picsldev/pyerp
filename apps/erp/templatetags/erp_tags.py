1360b046bd6b79ae7114add9bbc0f7d10c54e26b
odoo <falconsoft.3d@gmail.com>
2019-08-18 19:15:10 -0400

    tags Compañia

--- apps/erp/templatetags/erp_tags.py
+++ apps/erp/templatetags/erp_tags.py
@@ -30,3 +30,15 @@ def get_company_slogan(obj):
 @register.filter
 def get_company_rut(obj):
     return BaseConfig.objects.get(pk=1).main_company_id.rut
+
+@register.filter
+def get_company_facebook(obj):
+    return BaseConfig.objects.get(pk=1).main_company_id.social_facebook
+
+@register.filter
+def get_company_instagram(obj):
+    return BaseConfig.objects.get(pk=1).main_company_id.social_instagram
+
+@register.filter
+def get_company_linkedin(obj):
+    return BaseConfig.objects.get(pk=1).main_company_id.social_linkedin
