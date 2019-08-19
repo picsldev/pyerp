9913977719e7b2dae6857fcf3f76d3ea1876fa21
marlon <falconsoft.3d@gmail.com>
2019-08-19 00:57:20 -0400

    Ajustando Blog

--- apps/website/submodels/post.py
+++ apps/website/submodels/post.py
@@ -1,13 +1,15 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
+import datetime
 
 class PyPost(models.Model):
     name = models.CharField('Nombre', max_length=255)
     description = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
 
     def get_absolute_url(self):
-        return reverse('article-detail', kwargs={'pk': self.pk})
+        return reverse('post-detail', kwargs={'pk': self.pk})
 
     def __str__(self):
         return format(self.name)
