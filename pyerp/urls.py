98064e8aa468b0b727274e589d9b1dbf37f8df74
marlon <falconsoft.3d@gmail.com>
2019-08-19 01:29:45 -0400

    Creando Modelo Actividad Económica

--- pyerp/urls.py
+++ pyerp/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     url(r'^account/', include('apps.account.urls')),
     url(r'^pos/', include('apps.pos.urls')),
     url(r'^payroll/', include('apps.payroll.urls')),
+    url(r'^dte/', include('apps.dte.urls')),
 ]
