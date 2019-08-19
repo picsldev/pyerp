2a6abb585c12011fa2f0f1d485ddaecde8b3d44d
marlon <falconsoft.3d@gmail.com>
2019-08-19 00:35:43 -0400

    Separando el Módulo de Web

--- apps/website/urls.py
+++ apps/website/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from ..website.views import ArticleListView, ArticleDetailView, ArticleCreateView, ArticleUpdateView, DeleteArticle
+from .subviews.post import PostListView, PostDetailView, PostCreateView, PostUpdateView, DeletePost
 
 
 urlpatterns = [
-    path('article', ArticleListView.as_view(), name='article'),
-    path('article/add/', ArticleCreateView.as_view(), name='article-add'),
-    path('article/<int:pk>/', ArticleDetailView.as_view(), name='article-detail'),
-    path('article/<int:pk>/update', ArticleUpdateView.as_view(), name='article-update'),
-    path('article/<int:pk>/delete/', DeleteArticle, name='article-delete'),
+    path('post', PostListView.as_view(), name='post'),
+    path('post/add/', PostCreateView.as_view(), name='post-add'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', DeletePost, name='post-delete'),
 ]
