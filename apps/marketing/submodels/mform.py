087cc63dd666e1b849bb0726ba13592c61c01b74
marlon <falconsoft.3d@gmail.com>
2019-08-20 13:32:49 -0400

    Campaña y Canal de Venta

--- apps/marketing/submodels/mform.py
+++ apps/marketing/submodels/mform.py
@@ -2,9 +2,11 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
+from .campaign import PyCampaign
 
 class PyMform(models.Model):
     name = models.CharField('Nombre', max_length=255)
+    campaign_id = models.ForeignKey(PyCampaign, null=True, blank=True, on_delete=models.CASCADE)
 
     def get_absolute_url(self):
         return reverse('mform-detail', kwargs={'pk': self.pk})
