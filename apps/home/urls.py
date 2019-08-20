6ce8e87a99bc68b661c91768f3fada242d96861b
marlon <falconsoft.3d@gmail.com>
2019-08-19 21:45:43 -0400

    mejorando web

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 
-from ..home.views import index, blog, shop
+from ..home.views import index, blog, shop, post, license
 
 urlpatterns = [
     url(r'^$', index, name='home-index'),
     url(r'^blog/', blog, name='home-blog'),
     url(r'^shop/', shop, name='home-shop'),
-    url(r'^post/', shop, name='home-post'),
+    url(r'^post/', post, name='home-post'),
+    url(r'^license/', license, name='home-license'),
 ]
