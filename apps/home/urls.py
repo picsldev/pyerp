7aa946b1f3d7b87efaa779e19de21cd1bd7dd5e8
marlon <falconsoft.3d@gmail.com>
2019-08-26 22:56:13 -0400

    Iniciando con la tienda

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import url
 from django.urls import path
-from .subviews.views import index, BlogView, PostDetailView,  shop, UnderConstruction, product
+from .subviews.views import index, BlogView, PostDetailView,  WebProductView, UnderConstruction, product
 
 urlpatterns = [
     url(r'^$', index, name='home-index'),
     path('blog/', BlogView.as_view(), name='home-blog'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post'),
-    url(r'^shop/', shop, name='home-shop'),
+    url(r'^shop/', WebProductView.as_view(), name='home-shop'),
     url(r'^product/', product, name='home-product'),
     url(r'^license/', license, name='home-license'),
     url(r'^under-construction/', UnderConstruction, name='under-construction'),
