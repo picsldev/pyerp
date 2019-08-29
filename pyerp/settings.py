2f1347c7ff76224a599f15c9205cb7f25640de3e
marlon <falconsoft.3d@gmail.com>
2019-08-28 23:13:16 -0400

    Traduciendo

--- pyerp/settings.py
+++ pyerp/settings.py
@@ -120,7 +120,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'es-cl'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'America/Santiago'
 
