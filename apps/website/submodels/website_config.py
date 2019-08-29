908d5387763f31e8a49c9abb7563aaec892bbd6e
marlon <falconsoft.3d@gmail.com>
2019-08-29 01:47:47 -0400

    Trabajando en la Tienda

--- apps/website/submodels/website_config.py
+++ apps/website/submodels/website_config.py
@@ -1,14 +1,16 @@
 from django.db import models
 from django.urls import reverse
 from ...base.submodels.father import PyFather
+from django.utils.translation import ugettext_lazy as _
 
 
 
 class WebsiteConfig(PyFather):
-    show_blog = models.BooleanField('Mostrar Blog', default=False)
-    show_shop = models.BooleanField('Mostrar Tienda', default=False)
-    show_chat = models.BooleanField('Mostrar Chat', default=False)
-    under_construction = models.BooleanField('En Construcción', default=False)
+    show_blog = models.BooleanField(_("Show Blog"), default=False)
+    show_shop = models.BooleanField(_("Show Shop"), default=False)
+    show_price = models.BooleanField(_("Show price"), default=True)
+    show_chat = models.BooleanField(_("Show chat"), default=False)
+    under_construction = models.BooleanField(_("Under Construction"), default=False)
 
 
     def get_absolute_url(self):
