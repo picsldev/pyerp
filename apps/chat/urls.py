a71f55a72bd480a5508abbf43ff213aa232a6f36
carmelo_lopez <carmelolopez3@hotmail.com>
2019-08-30 19:00:29 -0400

    Avances.

--- apps/chat/urls.py
+++ apps/chat/urls.py
@@ -7,10 +7,14 @@ from django.urls import path
 from .subviews.trigger import (
     DeleteTrigger, TriggerCreateView, TriggerDetailView, TriggerListView,
     TriggersUpdateView)
-from .views import chat_home
+from .views import chat_home, get_by_sid, get_client_name, register_message, get_history
 
 urlpatterns = [
     url(r'chat-home', chat_home, name='chat-home'),
+    path('get_by_sid/<str:sid>', get_by_sid, name='get-by-sid'),
+    path('get_client_name/<str:sid>', get_client_name, name='get-client-name'),
+    path('get_history/<str:sid>', get_history),
+    path('register_message', register_message),
     path('triggers', TriggerListView.as_view(), name='triggers'),
     path('trigger/add/', TriggerCreateView.as_view(), name='trigger-add'),
     path('trigger/<int:pk>/', TriggerDetailView.as_view(), name='trigger-detail'),
--- apps/chat/views.py
+++ apps/chat/views.py
@@ -1,9 +1,40 @@
 # Librerias Django
+import json
+import datetime
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
-from django.shortcuts import render
+from django.shortcuts import HttpResponse, render
+from .models import PyVisitor, PyChatHistory
 
 
 @login_required(login_url="/erp/login")
 def chat_home(request):
     return render(request, "pychat.html")
+
+
+def get_client_name(self, sid):
+    visitor = PyVisitor.objects.filter(sid=sid).first()
+    return HttpResponse(json.dumps({
+        'name': visitor and visitor.name or 'No registrado %s' % sid[-5:]
+    }), content_type='application/json')
+
+
+def get_by_sid(self, sid):
+    return PyVisitor.objects.filter(sid=sid).first()
+
+
+def register_message(self, **kwargs):
+    sid = kwargs.get('sid')
+    message = kwargs.get('message')
+    date = datetime.datetime.strptime(kwargs.get('date'), '%Y-%m-%d %H:%M:%S')
+    visitor = PyVisitor.objects.filter(sid=sid).first()
+    if not visitor:
+        visitor = PyVisitor(sid=sid)
+        visitor.save()
+    chat_message = PyChatHistory(visitor=visitor, message=message, datetime=date, response=kwargs.get('response'))
+    chat_message.save()
+    return HttpResponse(b'Ok')
+
+
+def get_history(self, sid):
+    visitor = PyVisitor.objects.filter(sid=sid).first()
+    history = PyChatHistory.objects.filter(visitor=visitor)
