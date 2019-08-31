925bd5e2153d7a447fbcffe7fc416c2dd33b9bef
marlon <falconsoft.3d@gmail.com>
2019-08-30 20:50:06 -0400

    Do Traslate

---
+++ apps/account/info.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Part of PyERP. See LICENSE file for full copyright and licensing details.
+{
+	'name':'account',
+	'author': "YnexT SpA",
+	'website': 'www.ynext.cl',
+	'category': 'Accounting',
+	'summary': 'Account',
+	'depends': '',
+	'description': "",
+	'external_dependencies':"",
+	'type':'1'
+}
