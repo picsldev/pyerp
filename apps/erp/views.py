58d9847688938baa8f3e17d3b4b1e39a4aaa1eb0
marlon <falconsoft.3d@gmail.com>
2019-09-02 03:45:15 -0400

    Iniciando con la separacion

--- apps/erp/views.py
+++ apps/erp/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 from django.shortcuts import render
 
 # Librerias en carpetas locales
-from ..base.models import PyPartner, PyProduct
+from ..base.models import PyPartner, PyProduct, PyApp
 from .subviews.logoutmodal import LogOutModalView
 
 
@@ -14,10 +14,18 @@ from .subviews.logoutmodal import LogOutModalView
 def erp_home(request):
     """Vista para renderizar el dasboard del erp
     """
+    apps = PyApp.objects.all().filter(installed=True)
+    app_list = []
+    if apps:
+        for app in apps:
+            st = app.name + "/menu.html"
+            app_list.append(st.lower())
+
     partners = PyPartner.objects.all()
     return render(request, "home.html", {
         'customers': partners.filter(customer=True),
         'providers': partners.filter(provider=True),
         'users': User.objects.all(),
-        'products': PyProduct.objects.all()
+        'products': PyProduct.objects.all(),
+        'app_list' : app_list
     })
