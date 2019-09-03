7b204f1c88078309c3eb7712b2b2af6a7d49db82
marlon <falconsoft.3d@gmail.com>
2019-09-02 21:02:42 -0400

    Arreglando el conf

--- apps/base/views.py
+++ apps/base/views.py
@@ -126,9 +126,9 @@ def UpdateApps(self):
     PyApp.objects.all().delete()
     for folder in list_app:
         try:
-            for file in listdir(folder_apps + "/" + folder + "/py"):
+            for file in listdir(folder_apps + "/" + folder):
                 if file == FILE_NAME:
-                    with open(folder_apps + "/" + folder + "/py/" + FILE_NAME) as json_file:
+                    with open(folder_apps + "/" + folder + "/" + FILE_NAME) as json_file:
                         data = json.load(json_file)
                         p = PyApp(name=data['name'], description=data['description'], author=data['author'],
                                   fa=data['fa'], version=data['version'],
--- apps/chat/py/py_settings.py
+++
@@ -1,4 +0,0 @@
-try:
-    INSTALLED_APPS += 'apps.chat'
-except:
-    import logging; logging.getLogger(__name__).warn('Faltan las installed_apps del settings.py')
