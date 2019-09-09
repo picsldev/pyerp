e27d08941a6f4daafd38cbaf23fcb39377e896db
marlon <falconsoft.3d@gmail.com>
2019-09-09 20:02:08 -0300

    Error3

--- apps/home/models/__init__.py
+++ apps/home/models/__init__.py
@@ -1,3 +1,2 @@
 from .post import PyPost
-from .web_payment_methods import PyWebPaymentMethod
 from .website_config import PyWebsiteConfig
--- apps/home/urls.py
+++ apps/home/urls.py
@@ -34,10 +34,4 @@ urlpatterns = [
     path('post/<int:pk>/delete/', DeletePost, name='post-delete'),
 
     path('config/<int:pk>', UpdateWebsiteConfigView.as_view(), name='website-config'),
-
-    path('web-payment-methods', WebPaymentMethodListView.as_view(), name='web-payment-methods'),
-    path('web-payment-method/add/', WebPaymentMethodCreateView.as_view(), name='web-payment-method-add'),
-    path('web-payment-method/<int:pk>/', WebPaymentMethodDetailView.as_view(), name='web-payment-method-detail'),
-    path('web-payment-method/<int:pk>/update', WebPaymentMethodUpdateView.as_view(), name='web-payment-method-update'),
-    path('web-payment-method/<int:pk>/delete/', DeleteWebPaymentMethod, name='web-payment-method-delete'),
 ]
